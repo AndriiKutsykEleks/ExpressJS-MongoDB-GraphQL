30486b99fae1676dbef3ddd9f23110159ea71bd3
Anri1214 <Anri1214.K@gmail.com>
2019-06-03 00:45:23 +0300

    Fix check dependecies valid result

--- middlewares/dependency.js
+++ middlewares/dependency.js
@@ -2,7 +2,7 @@ const { errorService, validateService } = require('./../services');
 
 const dependencyMiddleware = (model, key) => {
     return async (req, res, next) => {
-        const valid = validateService.isDataExist(req, res, { model, key });
+        const valid = await validateService.isDataExist(req, res, { model, key });
         valid ? next() : errorService.sendError(res, `${key} value not exist`, 400);
     };
 };
